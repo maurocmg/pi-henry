708c5e62305d84c167f56ba3c662709e602f014a
maurocmg <mmelgarejo@jus.mendoza.gov.ar>
2023-05-11 13:08:17 -0300

    oficina

--- api/src/controllers/getVideogames.js
+++ api/src/controllers/getVideogames.js
@@ -9,28 +9,45 @@ const API_URL = 'https://api.rawg.io/api';
 // Hacemos petición a la API, y luego por cada juego recibido extraemos las propiedades que nos interesan. En el caso de platforms, también dejamos solo el nombre de las mismas. Pendiente de implementar mensaje de error.
 
 const getVideogames = async (req, res) => {
-    try {
-        
-        const response = await axios.get(`${API_URL}/games?key=${API_KEY}`); 
-        const videogames = response.data.results.map( (game) => {
-            return { 
-                id: game.id, 
-                name: game.name, 
-                description: game.description,
-                image: game.background_image,
-                released: game.released,
-                rating: game.rating, 
-                platforms: game.platforms.map( (platform) => {
-                    return {
-                        name: platform.platform.name
-                    }
-                })
-            }
-        });
-        res.status(200).json({videogames})
-    } catch (error) {
-        res.status(500).json('{message : errormessage}')
+    const idreq = Object.keys(req.query)[0]
+
+    if (idreq) {
+        try {
+            const response = await axios.get(`${API_URL}/games/${idreq}?key=${API_KEY}`);
+            const { id, name, description, background_image, released, rating, platforms } = response.data;
+            // cleanplatforms = platforms.map( (platform) => {
+            //     return {
+            //         name: platform.platform.name
+            //     }});
+            // res.status(200).json( { id, name, description, background_image, released, rating, platforms })
+            res.status(200).json({ id, name, description, background_image, released, rating, platforms })
+        } catch (error) {
+            res.status(500).json('{message : errormessage}')
+        }
+    } else {
+        try {        
+            const response = await axios.get(`${API_URL}/games?key=${API_KEY}`); 
+            const videogames = response.data.results.map( (game) => {
+                return { 
+                    id: game.id, 
+                    name: game.name, 
+                    description: game.description,
+                    background_image: game.background_image,
+                    released: game.released,
+                    rating: game.rating, 
+                    platforms: game.platforms.map( (platform) => {
+                        return {
+                            name: platform.platform.name
+                        }
+                    })
+                }
+            });
+            res.status(200).json({videogames})
+        } catch (error) {
+            res.status(500).json('{message : errormessage}')
+        }
     }
+
 }
 
 module.exports = getVideogames; 
