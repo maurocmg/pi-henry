c805f113b7425a5d8ae315951083b2c534988ae5
maurocmg <maurocmg@gmail.com>
2023-05-19 15:23:52 -0300

    paginado de gamelist

--- client/src/components/GameList/GameList.js
+++ client/src/components/GameList/GameList.js
@@ -20,23 +20,48 @@ const GameList = () => {
     fetchGames();
   }, []);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const gamesPerPage = 15;
+  const indexOfLastGame = currentPage * gamesPerPage;
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+  const currentGames = games.slice(indexOfFirstGame, indexOfLastGame);
+  
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalPages = Math.ceil(games.length / gamesPerPage);
+
   const chunk = (array, size) =>
     Array.from({ length: Math.ceil(array.length / size) }, (v, i) =>
       array.slice(i * size, i * size + size)
     );
 
   // Dividir los juegos en grupos de tres
-  const gameRows = chunk(games, 3);
+  const gameRows = chunk(currentGames, 3);
 
   return (
     <div>
-      {gameRows.map((row, rowIndex) => (
-        <div key={rowIndex} className={styles.gameRow}>
-          {row.map((game) => (
-            <Card key={game.id} game={game} />
-          ))}
+        <div>
+            {gameRows.map((row, rowIndex) => (
+                <div key={rowIndex} className={styles.gameRow}>
+                    {row.map((game) => (
+                    <Card key={game.id} game={game} />
+                    ))}
+                </div>
+            ))}
         </div>
-      ))}
+            <div className={styles.pageIndicator}>
+                {Array.from({ length: totalPages }, (_, index) => (
+                <button
+                key={index}
+                className={currentPage === index + 1 ? styles.active : ''}
+                onClick={() => handlePageChange(index + 1)}
+                >
+                {index + 1}
+                </button>
+                ))}
+        </div>
     </div>
   );
 
